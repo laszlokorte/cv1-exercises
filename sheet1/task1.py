76aa3187349952f01d2bd517d75b7bc31dd304fc
Laszlo Korte <9korte@informatik.uni-hamburg.de>
2022-10-28 11:54:03 +0200

    finish sheet 1

--- sheet1/task1.py
+++ sheet1/task1.py
@@ -8,7 +8,7 @@ def unpickle(file):
     return dict
 
 def to_gray_scale(batch):
-    return (batch[:,0:1024]+batch[:,1024:2048]+batch[:,2048:]) / 3
+    return ((batch[:,0:1024]+batch[:,1024:2048]+batch[:,2048:]) / 3)
 
 def generate_bins(min, max, num):
     return np.arange(min, max + max/num, max/num)
@@ -23,10 +23,8 @@ if __name__ == '__main__':
     training_batch = unpickle(training_batch_file)
     test_batch = unpickle(test_batch_file)
 
-    bins = generate_bins(0, 255, 51)
-
     training_labels_all = np.array(training_batch[b'labels'])
-    training_data_all = np.array(training_batch[b'data'])
+    training_data_all = np.array(training_batch[b'data']).astype('float32')
 
     training_labels_automobile = training_labels_all[training_labels_all==1][:30]
     training_labels_deer = training_labels_all[training_labels_all==4][:30]
@@ -40,17 +38,8 @@ if __name__ == '__main__':
     training_deer_gray = to_gray_scale(training_deer)
     training_ship_gray = to_gray_scale(training_ship)
 
-
-    training_automobile_hist = np.apply_along_axis(histogram, -1, training_automobile_gray, bins)
-    training_deer_hist = np.apply_along_axis(histogram, -1, training_deer_gray, bins)
-    training_ship_hist = np.apply_along_axis(histogram, -1, training_ship_gray, bins)
-    
-    training_data = np.concatenate((training_automobile_hist, training_deer_hist, training_ship_hist))
-    training_labels = np.concatenate((training_labels_automobile, training_labels_deer, training_labels_ship))
-
-
     test_labels_all = np.array(test_batch[b'labels'])
-    test_data_all = np.array(test_batch[b'data'])
+    test_data_all = np.array(test_batch[b'data']).astype('float32')
 
     test_labels_automobile = test_labels_all[test_labels_all==1][:30]
     test_labels_deer = test_labels_all[test_labels_all==4][:30]
@@ -64,39 +53,37 @@ if __name__ == '__main__':
     test_deer_gray = to_gray_scale(test_deer)
     test_ship_gray = to_gray_scale(test_ship)
 
-    test_automobile_hist = np.apply_along_axis(histogram, -1, test_automobile_gray, bins)
-    test_deer_hist = np.apply_along_axis(histogram, -1, test_deer_gray, bins)
-    test_ship_hist = np.apply_along_axis(histogram, -1, test_ship_gray, bins)
-    
-    test_data = np.concatenate((test_automobile_hist, test_deer_hist, test_ship_hist))
-    test_labels = np.concatenate((test_labels_automobile, test_labels_deer, test_labels_ship))
 
-    # plt.imshow(np.concatenate((test_deer[10,:1024].reshape(32,32,1), test_deer[10,1024:2048].reshape(32,32,1), test_deer[10,2048:].reshape(32,32,1) ), -1))
-    # plt.show()
-    # plt.imshow(np.concatenate((test_automobile[10,:1024].reshape(32,32,1), test_automobile[10,1024:2048].reshape(32,32,1), test_automobile[10,2048:].reshape(32,32,1) ), -1))
-    # plt.show()
-    # plt.imshow(np.concatenate((test_ship[10,:1024].reshape(32,32,1), test_ship[10,1024:2048].reshape(32,32,1), test_ship[10,2048:].reshape(32,32,1) ), -1))
-    # plt.show()
+    for number_of_bins in [2,10,51,255]:
+        bins = generate_bins(0, 255, number_of_bins)
 
-    count_true = 0
-    count_false = 0
-    for real_label, test_d in zip(test_labels, test_data):
-        min_distance = float('inf')
-        prediction = None
-        for training_label, training_d in zip(training_labels, training_data):
-            dist = np.linalg.norm(training_d - test_d)
-            if dist < min_distance:
-                min_distance = dist
-                prediction = training_label
-        if prediction == real_label:
-            count_true += 1
-        else:
-            count_false += 1
+        training_automobile_hist = np.apply_along_axis(histogram, -1, training_automobile_gray, bins)
+        training_deer_hist = np.apply_along_axis(histogram, -1, training_deer_gray, bins)
+        training_ship_hist = np.apply_along_axis(histogram, -1, training_ship_gray, bins)
+        
+        training_data = np.concatenate((training_automobile_hist, training_deer_hist, training_ship_hist))
+        training_labels = np.concatenate((training_labels_automobile, training_labels_deer, training_labels_ship))
 
-    print(f"correct: {count_true}, false: {count_false}")
-    #deer_hist = np.apply_along_axis(np.histogram, -1, deer_gray, bins)
-    #ship_hist = np.apply_along_axis(np.histogram, -1, ship_gray, bins)
-    # print(automobile_hist)
-    # print(batch[b'batch_label'])
-    # print(batch[b'labels'])
-    # print(batch[b'data'])
+        test_automobile_hist = np.apply_along_axis(histogram, -1, test_automobile_gray, bins)
+        test_deer_hist = np.apply_along_axis(histogram, -1, test_deer_gray, bins)
+        test_ship_hist = np.apply_along_axis(histogram, -1, test_ship_gray, bins)
+        
+        test_data = np.concatenate((test_automobile_hist, test_deer_hist, test_ship_hist))
+        test_labels = np.concatenate((test_labels_automobile, test_labels_deer, test_labels_ship))
+
+        count_true = 0
+        count_false = 0
+        for real_label, test_d in zip(test_labels, test_data):
+            min_distance = float('inf')
+            prediction = None
+            for training_label, training_d in zip(training_labels, training_data):
+                dist = np.linalg.norm(training_d - test_d)
+                if dist < min_distance:
+                    min_distance = dist
+                    prediction = training_label
+            if prediction == real_label:
+                count_true += 1
+            else:
+                count_false += 1
+
+        print(f"Bins: {number_of_bins}, correct: {count_true}, false: {count_false}")
--- sheet1/task2.py
+++ sheet1/task2.py
@@ -8,25 +8,27 @@ flower = io.imread('visual_attention_ds.png')
 flower_gray = rgb2gray(rgba2rgb(flower))
 flower_integral = transform.integral.integral_image(flower_gray)
 
-print(flower_gray)
-
 height, width = flower_gray.shape
 
-result = np.zeros_like(flower_gray)
-
-plt.imshow(flower_gray)
-plt.show()
-
 window_sizes = [
-	(11, 21),
 	(3, 7),
+	(11, 21),
 	(31, 51),
 ]
 
 def is_inside(inner, outer):
 	return outer[0] <= inner[0] < outer[1] and outer[0] <= inner[1] < outer[1]
 
-for inner, outer in window_sizes:
+fig, ax = plt.subplots(2, 3)
+
+ax[0, 0].imshow(flower)
+ax[0, 0].set_title("Original RGB")
+ax[0, 1].imshow(flower_gray, cmap='gray')
+ax[0, 1].set_title("Grayscale")
+ax[0, 2].set_visible(False)
+
+for i, (inner, outer) in enumerate(window_sizes):
+	result = np.zeros_like(flower_gray)
 	inner_half = int((inner - 1)/2)
 	outer_half = int((outer - 1)/2)
 	for row in range(height):
@@ -40,6 +42,8 @@ for inner, outer in window_sizes:
 					diff = surround_sum - center_sum
 			result[row,col] = diff
 
-	plt.imshow(result)
-	plt.show()
+	ax[1, i].set_title(f"Suround/Center: {outer}/{inner}")
+	ax[1, i].imshow(result, cmap='gray')
 
+fig.tight_layout()
+plt.show()
