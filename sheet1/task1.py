5462f9a601e94aed12f24a29909f42e6334e8681
Laszlo Korte <git@laszlokorte.de>
2022-11-01 19:30:55 +0100

    cleanup code

--- sheet1/task1.py
+++ sheet1/task1.py
@@ -1,5 +1,33 @@
 import numpy as np
-import matplotlib.pyplot as plt
+import os
+
+def main():
+    bin_sizes = [2,10,51,255]
+    categories = [1,4,8]
+    training_size = 30
+    test_size = 10
+
+    training_labels, training_data = read_batch(
+        os.path.join(os.path.dirname(__file__), 'cifar-10-batches-py', 'data_batch_1'), 
+        categories, training_size)
+    test_labels, test_data = read_batch(
+        os.path.join(os.path.dirname(__file__), 'cifar-10-batches-py', 'test_batch'), 
+        categories, test_size)
+
+    training_gray = to_gray_scale(training_data)
+    test_gray = to_gray_scale(test_data)
+
+    for number_of_bins in bin_sizes:
+        bins = make_bins(0, 255, number_of_bins)
+        
+        training_histograms = np.apply_along_axis(make_histogram, -1, training_data, bins)
+        test_histograms = np.apply_along_axis(make_histogram, -1, test_data, bins)
+        predictions = np.apply_along_axis(make_prediction, -1, test_histograms, training_histograms, training_labels)
+
+        correct = np.sum(predictions==test_labels)
+        incorrect = np.sum(predictions!=test_labels)
+
+        print(f"Bins: {number_of_bins}, correct: {correct}, false: {incorrect}")
 
 def unpickle(file):
     import pickle
@@ -7,83 +35,26 @@ def unpickle(file):
         dict = pickle.load(fo, encoding='bytes')
     return dict
 
-def to_gray_scale(batch):
-    return ((batch[:,0:1024]+batch[:,1024:2048]+batch[:,2048:]) / 3)
+def read_batch(batch_file, categories, per_category):
+    batch = unpickle(batch_file)
+    labels = np.array(batch[b'labels'])
+    data = np.array(batch[b'data'], dtype=np.float32)
 
-def generate_bins(min, max, num):
+    return np.concatenate([labels[labels == cat][:per_category] for cat in categories]), \
+           np.concatenate([data[labels == cat][:per_category] for cat in categories])
+
+def to_gray_scale(batch):
+    return (batch[:,0:1024]+batch[:,1024:2048]+batch[:,2048:]) / 3
+
+def make_bins(min, max, num):
     return np.arange(min, max + max/num, max/num)
 
-def histogram(data,bins): 
+def make_histogram(data, bins): 
     return np.histogram(data,bins=bins)[0]
 
+def make_prediction(single_histogram, training_batch, training_labels):
+    diff = training_batch - single_histogram
+    return training_labels[np.linalg.norm(diff, axis=1).argmin()]
+
 if __name__ == '__main__':
-    training_batch_file = './cifar-10-batches-py/data_batch_1'
-    test_batch_file = './cifar-10-batches-py/test_batch'
-
-    training_batch = unpickle(training_batch_file)
-    test_batch = unpickle(test_batch_file)
-
-    training_labels_all = np.array(training_batch[b'labels'])
-    training_data_all = np.array(training_batch[b'data']).astype('float32')
-
-    training_labels_automobile = training_labels_all[training_labels_all==1][:30]
-    training_labels_deer = training_labels_all[training_labels_all==4][:30]
-    training_labels_ship = training_labels_all[training_labels_all==8][:30]
-
-    training_automobile = training_data_all[training_labels_all==1][:30]
-    training_deer = training_data_all[training_labels_all==4][:30]
-    training_ship = training_data_all[training_labels_all==8][:30]
-
-    training_automobile_gray = to_gray_scale(training_automobile)
-    training_deer_gray = to_gray_scale(training_deer)
-    training_ship_gray = to_gray_scale(training_ship)
-
-    test_labels_all = np.array(test_batch[b'labels'])
-    test_data_all = np.array(test_batch[b'data']).astype('float32')
-
-    test_labels_automobile = test_labels_all[test_labels_all==1][:30]
-    test_labels_deer = test_labels_all[test_labels_all==4][:30]
-    test_labels_ship = test_labels_all[test_labels_all==8][:30]
-
-    test_automobile = test_data_all[test_labels_all==1][:30]
-    test_deer = test_data_all[test_labels_all==4][:30]
-    test_ship = test_data_all[test_labels_all==8][:30]
-
-    test_automobile_gray = to_gray_scale(test_automobile)
-    test_deer_gray = to_gray_scale(test_deer)
-    test_ship_gray = to_gray_scale(test_ship)
-
-
-    for number_of_bins in [2,10,51,255]:
-        bins = generate_bins(0, 255, number_of_bins)
-
-        training_automobile_hist = np.apply_along_axis(histogram, -1, training_automobile_gray, bins)
-        training_deer_hist = np.apply_along_axis(histogram, -1, training_deer_gray, bins)
-        training_ship_hist = np.apply_along_axis(histogram, -1, training_ship_gray, bins)
-        
-        training_data = np.concatenate((training_automobile_hist, training_deer_hist, training_ship_hist))
-        training_labels = np.concatenate((training_labels_automobile, training_labels_deer, training_labels_ship))
-
-        test_automobile_hist = np.apply_along_axis(histogram, -1, test_automobile_gray, bins)
-        test_deer_hist = np.apply_along_axis(histogram, -1, test_deer_gray, bins)
-        test_ship_hist = np.apply_along_axis(histogram, -1, test_ship_gray, bins)
-        
-        test_data = np.concatenate((test_automobile_hist, test_deer_hist, test_ship_hist))
-        test_labels = np.concatenate((test_labels_automobile, test_labels_deer, test_labels_ship))
-
-        count_true = 0
-        count_false = 0
-        for real_label, test_d in zip(test_labels, test_data):
-            min_distance = float('inf')
-            prediction = None
-            for training_label, training_d in zip(training_labels, training_data):
-                dist = np.linalg.norm(training_d - test_d)
-                if dist < min_distance:
-                    min_distance = dist
-                    prediction = training_label
-            if prediction == real_label:
-                count_true += 1
-            else:
-                count_false += 1
-
-        print(f"Bins: {number_of_bins}, correct: {count_true}, false: {count_false}")
+    main()
--- sheet1/task2.py
+++ sheet1/task2.py
@@ -1,49 +1,62 @@
-from skimage import io
-from skimage import transform
+import os
+from skimage import io, transform
 from skimage.color import rgba2rgb, rgb2gray
 import matplotlib.pyplot as plt
 import numpy as np
+from tqdm import tqdm
 
-flower = io.imread('visual_attention_ds.png')
-flower_gray = rgb2gray(rgba2rgb(flower))
-flower_integral = transform.integral.integral_image(flower_gray)
+def main():
+	window_sizes = [
+		(3, 7),
+		(11, 21),
+		(31, 51),
+	]
+	file_name = 'visual_attention_ds.png'
 
-height, width = flower_gray.shape
+	flower = io.imread(os.path.join(os.path.dirname(__file__), file_name))
+	flower_gray = rgb2gray(rgba2rgb(flower))
+	flower_integral = transform.integral.integral_image(flower_gray)
 
-window_sizes = [
-	(3, 7),
-	(11, 21),
-	(31, 51),
-]
+	height, width = flower_gray.shape
 
-def is_inside(inner, outer):
-	return outer[0] <= inner[0] < outer[1] and outer[0] <= inner[1] < outer[1]
+	rows, columns = np.indices((height, width))
 
-fig, ax = plt.subplots(2, 3)
+	def is_inside(inner, outer):
+		return (outer[0] <= inner[0]) & (inner[0] < outer[1]) & (outer[0] <= inner[1]) & (inner[1] < outer[1])
 
-ax[0, 0].imshow(flower)
-ax[0, 0].set_title("Original RGB")
-ax[0, 1].imshow(flower_gray, cmap='gray')
-ax[0, 1].set_title("Grayscale")
-ax[0, 2].set_visible(False)
+	fig, ax = plt.subplots(2, len(window_sizes))
 
-for i, (inner, outer) in enumerate(window_sizes):
-	result = np.zeros_like(flower_gray)
-	inner_half = int((inner - 1)/2)
-	outer_half = int((outer - 1)/2)
-	for row in range(height):
-		for col in range(width):
-			r0c, c0c, r1c, c1c = row - inner_half, col - inner_half, row + inner_half, col + inner_half
-			r0o, c0o, r1o, c1o = row - outer_half, col - outer_half, row + outer_half, col + outer_half
-			diff = 0
-			if is_inside((c0o, c1o), (0, width)) and is_inside((r0o, r1o), (0, height)):
-					center_sum = transform.integral.integrate(flower_integral, (r0c, c0c), (r1c, c1c))
-					surround_sum = transform.integral.integrate(flower_integral, (r0o, c0o), (r1o, c1o))
-					diff = surround_sum - center_sum
-			result[row,col] = diff
+	fig.suptitle("Sheet 1, Task 2: Surround-Center Contract")
+	ax[0, 0].imshow(flower)
+	ax[0, 0].set_title("Original RGB")
+	ax[0, 1].imshow(flower_gray, cmap='gray')
+	ax[0, 1].set_title("Grayscale")
+	for x in range(2, len(window_sizes)):
+		ax[0, x].set_visible(False)
 
-	ax[1, i].set_title(f"Suround/Center: {outer}/{inner}")
-	ax[1, i].imshow(result, cmap='gray')
+	print("Calculating Center/Surround Contrast")
 
-fig.tight_layout()
-plt.show()
+	for i, (inner, outer) in enumerate(tqdm(window_sizes)):
+		result = np.zeros_like(flower_gray)
+		inner_half = int((inner - 1)/2)
+		outer_half = int((outer - 1)/2)
+
+		r0c, c0c, r1c, c1c = rows - inner_half, columns - inner_half, rows + inner_half, columns + inner_half
+		r0o, c0o, r1o, c1o = rows - outer_half, columns - outer_half, rows + outer_half, columns + outer_half
+
+		cutout = is_inside((c0o, c1o), (0, width)) & is_inside((r0o, r1o), (0, height))
+
+		center_sum = transform.integral.integrate(flower_integral, np.array(list(zip(r0c[cutout], c0c[cutout]))), np.array(list(zip(r1c[cutout], c1c[cutout]))))
+		surround_sum = transform.integral.integrate(flower_integral, np.array(list(zip(r0o[cutout], c0o[cutout]))), np.array(list(zip(r1o[cutout], c1o[cutout]))))
+		diff = surround_sum - center_sum
+		result[cutout] = diff
+
+		ax[1, i].set_title(f"Suround/Center: {outer}/{inner}")
+		ax[1, i].imshow(result, cmap='gray')
+
+	fig.tight_layout()
+	plt.show()
+
+if __name__ == '__main__':
+	main()
+
