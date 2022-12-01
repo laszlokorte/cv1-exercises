e01804140b5f2279f6344da898ac07806bc53ebc
Laszlo Korte <git@laszlokorte.de>
2022-12-01 01:00:13 +0100

    add note about clip(C-S)

--- sheet2/task3.py
+++ sheet2/task3.py
@@ -1,20 +1,6 @@
-# Gruppe 26
-# Laszlo Korte
-# Alexander Remmes-Weitz
-#
-# What advantage does using this approach based on image pyramids have over the integral
-# image method from assignment sheet 1?
-# When applying a filter to a signal the computational complexity depends both on the 
-# filter size and on the signal size.
-# So instead of applying multiple increasing filter sizes to a single large image
-# it is more efficient to apply a single smaller filter to multiple smaller images.
-# For the result only the relative size between the image and the filter matters.
-# The gaussian filter is a lowpass filter so we know we can discard high frequencies anyway
-# by downsampling the image.
-
 import os
 import numpy as np
-from skimage import io, transform
+from skimage import io
 from skimage.color import rgba2rgb, rgb2gray
 from skimage.transform import pyramid_gaussian, resize
 
@@ -22,8 +8,6 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 
 def main():
-	print(os.path.basename(__file__))
-	print("".join(open(__file__, 'r').readlines()[0:13]))
 	image_name = 'visual_attention.png'
 	image = io.imread(os.path.join(os.path.dirname(__file__), image_name))
 	image_gray = rgb2gray(rgba2rgb(image))
@@ -44,9 +28,16 @@ def main():
 	all_layers = np.stack((on_off_avg, off_on_avg))
 	all_layers_average = all_layers.mean(0)
 
-	plot_all(image, image_pyramid_center, image_pyramid_surround, on_off_contrast, off_on_contrast, on_off_avg, off_on_avg, all_layers_average)
+	# alternatively skip calculating the on_off and off_on contrast separately:
+	# mean[clip(a-b), clip(b-a)] =  abs(b-a)/2
+	abs_contrast = [np.abs(c-s)/2 for c,s in zip(image_pyramid_center, image_pyramid_surround)]
+	abs_contrast_upsampled = np.array([resize(l, image_gray.shape) for l in abs_contrast])
+	abs_avg = abs_contrast_upsampled.mean(0)
 
-def plot_all(image, image_pyramid_center, image_pyramid_surround, on_off_contrast, off_on_contrast, on_off_avg, off_on_avg, all_layers_average):
+	plot_all(image, image_pyramid_center, image_pyramid_surround, on_off_contrast, off_on_contrast, on_off_avg, off_on_avg, all_layers_average, abs_avg)
+
+
+def plot_all(image, image_pyramid_center, image_pyramid_surround, on_off_contrast, off_on_contrast, on_off_avg, off_on_avg, all_layers_average, abs_avg):
 	fig, ax = plt.subplots(5, 5, figsize=(16,12))
 	fig.suptitle("Sheet 2, Task 3: Image Pyramids")
 	
@@ -68,6 +59,7 @@ def plot_all(image, image_pyramid_center, image_pyramid_surround, on_off_contras
 	plot_image_gray(ax[3, 4], on_off_avg, f'on/off avg.')
 	plot_image_gray(ax[4, 4], off_on_avg, f'off/on avg.')
 	plot_image_gray(ax[0, 4], all_layers_average, f'conspicuity')
+	plot_image_gray(ax[0, 3], abs_avg, f'conspicuity(abs)')
 
 	for a in range(1, 5):
 		ax[a, 0].xaxis.set_major_locator(ticker.NullLocator())
@@ -84,12 +76,15 @@ def plot_image_gray(ax, image, label):
 	ax.set_title(f"{label}, {format_shape(image.shape)}")
 	ax.set_visible(True)
 
+
 def format_shape(s):
 	return "x".join(map(str, s))
+
 
 def hide_all_axis(ax):
 	for a in ax.reshape(-1):
 		a.set_visible(False)
+
 
 def add_extra_labels(ax):
 	ax[1, 0].set_ylabel("C", rotation=0, fontsize=15, labelpad=40,loc='center')
@@ -99,4 +94,3 @@ def add_extra_labels(ax):
 
 if __name__ == '__main__':
 	main()
-
