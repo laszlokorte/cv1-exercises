9aae1bc5f12f23e83d480d2157d5225540d836f7
Laszlo Korte <me@laszlokorte.de>
2022-12-14 19:40:25 +0100

    prepare for presentation

--- sheet3/task1.py
+++ sheet3/task1.py
@@ -3,6 +3,7 @@
 # Alexander Remmes-Weitz
 
 import os
+import math
 import numpy as np
 from skimage import io, transform, feature
 from skimage.util import random_noise
@@ -30,30 +31,41 @@ def main():
 	hough_spaces = np.concatenate([transform.hough_circle(image_edges, size/2) for (name, size) in coins_in_px])
 	radii = [size/2 for (name, size) in coins_in_px]
 	peaks = transform.hough_circle_peaks(hough_spaces, radii, num_peaks=2, total_num_peaks=10, normalize=True)
+	accums, cxs, cys, rads = peaks
 
-
-	fig, ax = plt.subplots(len(coins_in_px), 2, figsize=(8,10))
+	number_of_coins = len(coins_in_px)
+	number_of_figures = number_of_coins + 2
+	columns = math.ceil(number_of_figures/2)
+	fig, ax = plt.subplots(2, columns,  figsize=(12,5))
+	hide_axis(ax)
 	fig.suptitle("Sheet 3, Task 1: Hough transform")
 	
 	ax[0, 0].imshow(image_gray, cmap='gray')
 	ax[0, 0].set_title(f"Coins Gray & Results")
-	ax[1, 0].imshow(image_edges, cmap='gray')
-	ax[1, 0].set_title(f"Coins Edges")
-	ax[2, 0].set_visible(False)
-	ax[3, 0].set_visible(False)
-	ax[4, 0].set_visible(False)
+	ax[0, 0].set_visible(True)
+	ax[0, 1].imshow(image_edges, cmap='gray')
+	ax[0, 1].set_title(f"Coins Edges")
+	ax[0, 1].set_visible(True)
 
 	for i, hough in enumerate(hough_spaces):
-		ax[i, 1].imshow(hough, cmap='gray')
-		ax[i, 1].set_title(f"Hough Space ({coins_in_px[i][0]}, r={coins_in_px[i][1]/2}px)")
+		cell = i + 2
+		ax[cell//columns, cell%columns].imshow(hough, cmap='gray')
+		ax[cell//columns, cell%columns].set_title(f"Hough Space ({coins_in_px[i][0]}, r={coins_in_px[i][1]/2}px)")
+		ax[cell//columns, cell%columns].set_visible(True)
+		circle = plt.Circle((cxs[i], cys[i]), 10, edgecolor='cyan', facecolor='none')
+		ax[cell//columns, cell%columns].add_patch(circle)
 
-	accum, cx, cy, rad = peaks
-	for (accum, cx, cy, rad) in zip(accum, cx, cy, rad):
+	for (accum, cx, cy, rad) in zip(accums, cxs, cys, rads):
 		circle = plt.Circle((cx, cy), rad, edgecolor='r', facecolor='none')
 		ax[0, 0].add_patch(circle)
 
 	fig.tight_layout()
 	plt.show()
+
+def hide_axis(ax):
+	for a in ax:
+		for x in a:
+			x.set_visible(False)
 
 if __name__ == '__main__':
 	main()
