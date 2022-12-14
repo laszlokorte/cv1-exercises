a379a479f89cfdc3ad0471a75900e72677478fe4
Laszlo Korte <me@laszlokorte.de>
2022-12-14 19:51:18 +0100

    rearrange layout

--- sheet3/task1.py
+++ sheet3/task1.py
@@ -14,9 +14,11 @@ from tqdm import tqdm
 
 
 def main():
+	# Load Image
 	image_name = 'coins.jpg'
 	image_gray = io.imread(os.path.join(os.path.dirname(__file__), image_name), as_gray=True)
 	
+	# Configure expected coin sizes
 	image_res_mm_per_px = 0.12
 	coins_in_mm = [
 		('10marks', 27.25),
@@ -26,44 +28,50 @@ def main():
 		('10penni', 16.30),
 	]
 	coins_in_px = [(name, int(size/image_res_mm_per_px)) for (name, size) in coins_in_mm]
-
-	image_edges = feature.canny(image_gray)
-	hough_spaces = np.concatenate([transform.hough_circle(image_edges, size/2) for (name, size) in coins_in_px])
 	radii = [size/2 for (name, size) in coins_in_px]
-	peaks = transform.hough_circle_peaks(hough_spaces, radii, num_peaks=2, total_num_peaks=10, normalize=True)
-	accums, cxs, cys, rads = peaks
 
+	# Apply Hough-Transform:
+	# 1. Edge Detection
+	image_edges = feature.canny(image_gray)
+	# 2. Buildd Hough-Space
+	hough_spaces = np.concatenate([transform.hough_circle(image_edges, size/2) for (name, size) in coins_in_px])
+	# 3. Search for peaks in Hough-Space
+	accums, cxs, cys, rads = transform.hough_circle_peaks(hough_spaces, radii, num_peaks=2, total_num_peaks=10, normalize=True)
+
+	# Setup Plots
 	number_of_coins = len(coins_in_px)
-	number_of_figures = number_of_coins + 2
-	columns = math.ceil(number_of_figures/2)
-	fig, ax = plt.subplots(2, columns,  figsize=(12,5))
-	hide_axis(ax)
+	columns = math.ceil(number_of_coins/2)
+	fig, axs = plt.subplots(2, columns+1,  figsize=(12,5))
+	hide_axis(axs)
 	fig.suptitle("Sheet 3, Task 1: Hough transform")
 	
-	ax[0, 0].imshow(image_gray, cmap='gray')
-	ax[0, 0].set_title(f"Coins Gray & Results")
-	ax[0, 0].set_visible(True)
-	ax[0, 1].imshow(image_edges, cmap='gray')
-	ax[0, 1].set_title(f"Coins Edges")
-	ax[0, 1].set_visible(True)
+	# Plot original image and edges
+	axs[0, 0].imshow(image_gray, cmap='gray')
+	axs[0, 0].set_title(f"Coins Gray & Results")
+	axs[0, 0].set_visible(True)
+	axs[1, 0].imshow(image_edges, cmap='gray')
+	axs[1, 0].set_title(f"Coins Edges")
+	axs[1, 0].set_visible(True)
 
+	# Plot Hough-Spaces and peaks
 	for i, hough in enumerate(hough_spaces):
-		cell = i + 2
-		ax[cell//columns, cell%columns].imshow(hough, cmap='gray')
-		ax[cell//columns, cell%columns].set_title(f"Hough Space ({coins_in_px[i][0]}, r={coins_in_px[i][1]/2}px)")
-		ax[cell//columns, cell%columns].set_visible(True)
-		circle = plt.Circle((cxs[i], cys[i]), 10, edgecolor='cyan', facecolor='none')
-		ax[cell//columns, cell%columns].add_patch(circle)
+		ax = axs[i//columns, i%columns + 1]
+		ax.imshow(hough, cmap='gray')
+		ax.set_title(f"Hough Space ({coins_in_px[i][0]}, r={coins_in_px[i][1]/2}px)")
+		ax.set_visible(True)
+		peak = plt.Circle((cxs[i], cys[i]), 10, edgecolor='cyan', facecolor='none')
+		ax.add_patch(peak)
 
+	# Highlight results in original image
 	for (accum, cx, cy, rad) in zip(accums, cxs, cys, rads):
 		circle = plt.Circle((cx, cy), rad, edgecolor='r', facecolor='none')
-		ax[0, 0].add_patch(circle)
+		axs[0, 0].add_patch(circle)
 
 	fig.tight_layout()
 	plt.show()
 
-def hide_axis(ax):
-	for a in ax:
+def hide_axis(axs):
+	for a in axs:
 		for x in a:
 			x.set_visible(False)
 
